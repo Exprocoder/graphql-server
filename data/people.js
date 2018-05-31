15293ff96f500aeba9a876ab372c4e00aab063eb
ömer bayrak <omerbayrak1995@gmail.com>
2018-05-31 17:27:55 +0300

    commit

---
+++ data/people.js
@@ -0,0 +1,20 @@
+exports.data = [{
+    id: 1,
+    name: "Ömer",
+    surname: "Bayrak",
+    phone: "05457607225",
+    age: "22"
+}, {
+    id: 2,
+    name: "Ali",
+    surname: "Akar",
+    phone: "0545745896",
+    phone: "0547854252",
+    age: 23
+}, {
+    id: 3,
+    name: "Ayla",
+    surname: "Bayrak",
+    phone: "05885425548",
+    age: "45"
+}];
